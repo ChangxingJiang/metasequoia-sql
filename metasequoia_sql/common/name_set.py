e04a13d7d3f09a874ed61bdd40c1f488d46369a9
changxing <1278729001@qq.com>
2024-05-04 13:45:41 +0800

    修复 COUNT 窗口函数解析报错的 Bug

--- metasequoia_sql/common/name_set.py
+++ metasequoia_sql/common/name_set.py
@@ -5,8 +5,9 @@
 __all__ = ["WINDOW_FUNCTION_NAME_SET", "AGGREGATION_FUNCTION_NAME_SET", "GLOBAL_VARIABLE_NAME_SET"]
 
 # 窗口函数名称集合
-WINDOW_FUNCTION_NAME_SET = {"ROW_NUMBER", "RANK", "DENSE_RANK", "SUM", "MIN", "MAX", "AVG", "LAG", "LEAD",
-                            "FIRST_VALUE", "LAST_VALUE", "NTILE"}
+WINDOW_FUNCTION_NAME_SET = {"ROW_NUMBER", "RANK", "DENSE_RANK",
+                            "SUM", "MIN", "MAX", "AVG", "COUNT",
+                            "LAG", "LEAD", "FIRST_VALUE", "LAST_VALUE", "NTILE"}
 
 # 聚合函数名称集合
 AGGREGATION_FUNCTION_NAME_SET = {"COUNT", "SUM", "MIN", "MAX", "AVG"}
