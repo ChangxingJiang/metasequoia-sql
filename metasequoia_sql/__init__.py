ac64605bc122771901587867dcefc2c420411cc9
changxing <1278729001@qq.com>
2025-06-27 08:39:00 +0800

    【V1】更新模块引用

--- metasequoia_sql/__init__.py
+++ metasequoia_sql/__init__.py
@@ -1,5 +1,11 @@
+# pylint: disable=E0401,E0611
+
 """
 水杉 SQL 解析器
+
+屏蔽 pylint E0401：parser 将对外暴露
+屏蔽 pylint E0611：parser 文件将在编译阶段生成
 """
 
 from metasequoia_sql.lexical import LexFSM
+from metasequoia_sql.syntax import parse
